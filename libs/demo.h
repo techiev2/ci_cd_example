6bf9791ab75b575fed64e91270b5f14cf534d33f
Sriram Velamur <sriram.velamur@gmail.com>
2025-10-26 13:31:18 +0530

    Removes demo lib/function call in main.

--- libs/demo.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-void demo_start() {
-  printf("{\"message\": \"Demo started again.\"}");
-}
--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "libs/http.h"
-#include "libs/demo.h"
 
 int main(int argc, char** argv) {
   if (argc < 2) {
     fprintf(stderr, "{\"error\": true, \"message\": \"No URLs provided.\"}\n");
     return 1;
   }
-  demo_start();
   process_urls(argc, argv);
   return 0;
 }
