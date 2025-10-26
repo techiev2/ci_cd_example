4d3599529278251da589c012dfb113ad53904265
Sriram Velamur <sriram.velamur@gmail.com>
2025-10-26 13:32:30 +0530

    Changes process_urls output message.

--- libs/http.h
+++ libs/http.h
@@ -1,8 +1,17 @@
 #pragma once
 #include <stdio.h>
+#include <sys/time.h>
+
+int64_t get_current_time_ms() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (int64_t)tv.tv_sec * 1000 + (int64_t)tv.tv_usec / 1000;
+}
+
 void process_urls(int argc, char **argv) {
   unsigned int i = 1;
   for (; i < argc; i++) {
-    printf("{\"url\": \"%s\"}\n", argv[i]);
+    int64_t timestamp = get_current_time_ms();
+    printf("{\"message\": \"Procesing %s\", \"timestamp\": %lld}\n", argv[i], timestamp);
   }
 }
