ade5e403feec82a469f28beaa711424f04a83430
Sriram Velamur <sriram.velamur@gmail.com>
2025-10-28 20:24:21 +0530

    Adds thread based processing.

--- libs/http.h
+++ libs/http.h
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdint.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void* process_url(void* arg);
 
 int64_t get_current_time_ms() {
   struct timeval tv;
@@ -11,8 +15,18 @@ int64_t get_current_time_ms() {
 
 void process_urls(int argc, char **argv) {
   unsigned int i = 1;
+  pthread_t *threads = malloc(sizeof(pthread_t) * (argc - 1));
   for (; i < argc; i++) {
-    int64_t timestamp = get_current_time_ms();
-    printf("{\"message\": \"Procesing %s\", \"timestamp\": %lld}\n", argv[i], (long long)timestamp);
+    pthread_create(&threads[i-1], NULL, process_url, (void*) argv[i]);
   }
+  for (i = 0; i< argc - 1; i++) {
+    pthread_join(threads[i], NULL);
+  }
+}
+
+void * process_url(void* arg) {
+  char* url = arg;
+  int64_t timestamp = get_current_time_ms();
+  printf("{\"message\": \"Procesing %s\", \"timestamp\": %lld}\n", url, (long long)timestamp);
+  return NULL;
 }
