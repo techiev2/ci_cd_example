7306c07a3617efd416fa1a1fd3cbc8ab30a676d0
Sriram Velamur <sriram.velamur@gmail.com>
2025-10-30 23:41:36 +0530

    Switch to fputs for bounds safety.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv) {
   if (argc < 2) {
-    fprintf(stderr, "{\"error\": true, \"message\": \"No URLs provided.\"}\n");
+    fputs("{\"error\": true, \"message\": \"No URLs provided.\"}\n", stderr);
     return 1;
   }
   process_urls(argc, argv);
